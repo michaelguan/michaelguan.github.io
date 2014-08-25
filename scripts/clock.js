555297cacac2827df554d2a0eb3259e4b129f1d6
michaelguan <360744493@qq.com>
2014-08-25 21:48:19 +0800

    refact the clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,63 +1,111 @@
-function drawClock(canvas,ctx){ 
- var date=new Date();
- var clockRadius=Math.min(canvas.width/2,canvas.height/2);
- var hours=date.getHours();
- var minutes=date.getMinutes();
- var seconds=date.getSeconds();
- hours=hours>12?hours-12:hours;
- var hour=hours+minutes/60;
- var minute=minutes+seconds/60;
- 
- ctx.save();
- ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
- ctx.translate(canvas.width/2,canvas.height/2);
- ctx.scale(0.9,0.9);
- ctx.beginPath();
- ctx.font="36px Arial";
- ctx.fillStyle="#000";
- ctx.textAlign="center";
- ctx.textBaseline="middle";
- for(var n=1;n<=12;n++)
- { 
- var theta=(n-3)*2*Math.PI/12;
-  var x=clockRadius*0.7*Math.cos(theta);
-  var y=clockRadius*0.7*Math.sin(theta);
-  ctx.closePath();
-  ctx.fillText(n,x,y);
-  }   
-  ctx.save();
- var theta=(hour-3)*2*Math.PI/12;
- ctx.rotate(theta);
- ctx.beginPath();
- ctx.moveTo(-15,-5);
- ctx.lineTo(-15,5);
- ctx.lineTo(clockRadius*0.5,1);
- ctx.lineTo(clockRadius*0.5,-1);
- ctx.closePath();
- ctx.fill();
- ctx.restore();
-  ctx.save();
- var theta=(minute-15)*2*Math.PI/60;
- ctx.rotate(theta);
- ctx.beginPath();
- ctx.moveTo(-15,-4);
- ctx.lineTo(-15,4);
- ctx.lineTo(clockRadius*0.7,1);
- ctx.lineTo(clockRadius*0.7,-1);
- ctx.closePath();
- ctx.fill();
- ctx.restore();
-  ctx.save();
- var theta=(seconds-15)*2*Math.PI/60;
- ctx.rotate(theta);
- ctx.beginPath();
- ctx.moveTo(-15,-3);
- ctx.lineTo(-15,3);
- ctx.lineTo(clockRadius*0.9,1);
- ctx.lineTo(clockRadius*0.9,-1);
- ctx.fillStyle="#f00";
- ctx.closePath();
- ctx.fill();
- ctx.restore();
-  ctx.restore();
+function drawClock(canvas,context){
+     ///得到当前系统时间的：时、分、秒
+      var now_date=new Date();
+      var radius = Math.min(canvas.width/2,canvas.height/2),
+           sec=now_date.getSeconds(),
+           min=now_date.getMinutes(),
+           hour=now_date.getHours();
+           hour=hour>=12?hour-12:hour;
+     
+      //初始化画布
+      context.save();
+      context.clearRect(0,0,canvas.width,canvas.height);    
+      context.translate(canvas.width/2,canvas.height/2);        
+      context.scale(0.9,0.9);
+	  
+	  context.beginPath();
+	    context.textAlign="center";
+       context.textBaseline="middle";
+	   for(var n=1;n<=12;n++)
+       { 
+	    context.fillStyle="white";
+	   context.font="20px Arial";
+        var theta=(n-3)*2*Math.PI/12;
+        var x=radius*0.7*Math.cos(theta);
+        var y=radius*0.7*Math.sin(theta);
+		if(n%3==0)
+		{
+		  context.fillStyle="red";
+		  context.font="25px Arial";
+		}
+        context.closePath();
+        context.fillText(n,x,y);
+        }   
+      context.rotate(-Math.PI/2);
+      context.save();
+
+      //小时刻度
+      context.strokeStyle="white";
+      context.fillStyle="white";
+      context.lineWidth=3;
+      context.lineCap="round";
+      context.beginPath();
+      for(var i=0;i<12;i++){
+        context.rotate(Math.PI/6);
+        context.moveTo(radius-30,0);
+        context.lineTo(radius-10,0);
+      }
+      context.stroke();
+      context.restore();
+      context.save();
+     
+      //分钟刻度
+	  context.strokeStyle="white";
+      context.lineWidth=2;
+      context.beginPath();
+      for(var i=0;i<60;i++){
+        if(i%5!=0){
+          context.moveTo(radius-15,0);
+          context.lineTo(radius-10,0);
+        }
+        context.rotate(Math.PI/30);
+      }
+      context.stroke();
+      context.restore();
+      context.save();
+     
+      //画上时针
+      context.rotate((Math.PI/6)*hour+(Math.PI/360)*min+(Math.PI/21600)*sec);
+	  context.strokeStyle="green";
+      context.lineWidth=6;
+      context.lineCap="butt";
+      context.beginPath();
+      context.moveTo(-10,0);
+      context.lineTo(radius*0.5	,0);
+      context.stroke();
+      context.restore();
+      context.save();
+     
+      //分针
+      context.rotate((Math.PI/30)*min+(Math.PI/1800)*sec);
+      context.strokeStyle="#29A8DE";
+      context.lineWidth=4;
+      context.lineCap="butt";
+      context.beginPath();
+      context.moveTo(-20,0);
+      context.lineTo(radius*0.6,0);
+      context.stroke();
+      context.restore();
+      context.save();
+     
+      //秒针
+      context.rotate(sec*Math.PI/30);
+      context.strokeStyle="yellow";
+      context.lineWidth=2;
+      context.lineCap="butt";
+      context.beginPath();
+      context.moveTo(-30,0);
+      context.lineTo(radius*0.7,0);
+      context.stroke();
+      context.restore();
+      context.save();
+     
+      ///表框      
+      context.lineWidth=4;
+      context.strokeStyle="#882341";
+      context.beginPath();
+      context.arc(0,0,radius,0,Math.PI*2,true);
+      context.stroke();
+      context.restore();
+      context.restore();
 }
