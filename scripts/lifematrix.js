7ca855409d1cb607a88c39302edc9f9fffb96644
michaelguan <guan_tao@outlook.com>
2014-08-31 00:07:17 +0800

    improve the lifematrix.js

--- scripts/lifematrix.js
+++ scripts/lifematrix.js
@@ -1,6 +1,8 @@
 function drawLifeMatrix(canvas,context)
 {
-	context.strokeStyle="red";
+    context.clearRect(0,0,canvas.width,canvas.height); 
+	context.strokeStyle="white";
+	context.fillStyle="red";
     context.lineWidth=0.8;
      var i,j;
     for(i=0;i<=canvas.width;i+=20)
@@ -17,5 +19,22 @@ function drawLifeMatrix(canvas,context)
 	   context.moveTo(0,i);
 	   context.lineTo(canvas.width,i);
 	   context.stroke();		
+	}
+	
+	context.fillRect(2,2,18,18);
+	context.fillRect(2,22,18,18);
+	
+	var now_date=new Date();
+	var year=now_date.getFullYear();
+	var sec=now_date.getSeconds();
+	var month=now_date.getMonth()+1;
+	
+	for(i=1;i<=(year-1986)*12+month;i++)
+	{
+	      if(i==(year-1986)*12+month)
+		  {
+				context.fillStyle=sec%2?"yellow":"green";
+		  }
+		  context.fillRect(20*parseInt(((i-1)/24))+2,20*((i-1)%24)+2,18,18);
 	}
 }
