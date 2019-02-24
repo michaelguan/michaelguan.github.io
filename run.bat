87af88f7b2528773a7cc1b9f20ed9d00c58f21cf
hz-guantao <360744493@qq.com>
2019-02-24 23:57:52 +0800

    23:57:52.16

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
-echo %time% start push to github
-git add .>>github.log
-git commit -a -m %time%>>github.log
-git push>>github.log
-echo %time% push to github complete
+echo %time% start push to github >>github.log
+git add . >>github.log
+git commit -a -m %time% >>github.log
+git push >>github.log
+echo %time% push to github complete >>github.log
