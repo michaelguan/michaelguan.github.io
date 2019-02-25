98b8ff4ae4589223291039011ae71d38d64b575b
hz-guantao <360744493@qq.com>
2019-02-25 22:33:00 +0800

    update 20190225 22:32:59.86

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 cd /d %~dp0
 echo %Date:~0,4%%Date:~5,2%%Date:~8,2% %time% start push to github >>github.log
 git add . >>github.log
-git commit -a -m "update %date% %time%"  >>github.log
+git commit -a -m "update %Date:~0,4%%Date:~5,2%%Date:~8,2% %time%"  >>github.log
 git push >>github.log
 echo %Date:~0,4%%Date:~5,2%%Date:~8,2% %time% push to github complete >>github.log
+echo. >>github.log
